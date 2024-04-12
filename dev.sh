48fc443ef731f8b42edda5b32971a01a90726872
Oscar Beaumont <oscar@otbeaumont.me>
2024-04-13 02:36:30 +0800

    Fix docker build in CI

--- dev.sh
+++ dev.sh
@@ -13,11 +13,12 @@ pnpm -C web build
 
 if [ "$(uname)" == "Linux" ]; then
     cargo build --release
+    cp ./target/release/cityscale ./docker/cityscale
 else
     cargo zigbuild --target x86_64-unknown-linux-gnu --release
+    cp ./target/x86_64-unknown-linux-gnu/release/cityscale ./docker/cityscale
 fi
 
-cp ./target/x86_64-unknown-linux-gnu/release/cityscale ./docker/cityscale
 docker build --platform linux/amd64 -t "$DOCKER_TAG" ./docker
 
 if [ "$1" == "run" ]; then
